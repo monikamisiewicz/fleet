d75d7077c6c8778ed4406be99a2212d6f14e70c1
Monika Misiewicz <zieba.monika@wp.pl>
2020-06-01 22:47:26 +0200

    create function to display table in country.html

--- src/main/java/com/monikamisiewicz/fleet/controllers/CountryController.java
+++ src/main/java/com/monikamisiewicz/fleet/controllers/CountryController.java
@@ -1,13 +1,25 @@
 package com.monikamisiewicz.fleet.controllers;
 
+import com.monikamisiewicz.fleet.models.Country;
+import com.monikamisiewicz.fleet.services.CountryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class CountryController {
 
+    private final CountryService countryService;
+
     @GetMapping("/countries")
-    public String getCountries() {
+    public String getCountries(Model model) {
+
+        List<Country> countryList = countryService.getCountries();
+        model.addAttribute("countries", countryList);
         return "country";
     }
 
--- src/main/java/com/monikamisiewicz/fleet/services/CountryService.java
+++ src/main/java/com/monikamisiewicz/fleet/services/CountryService.java
@@ -1,7 +1,19 @@
 package com.monikamisiewicz.fleet.services;
 
+import com.monikamisiewicz.fleet.models.Country;
+import com.monikamisiewicz.fleet.repositories.CountryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class CountryService {
+
+    private final CountryRepository countryRepository;
+
+    public List<Country> getCountries() {
+        return countryRepository.findAll();
+    }
 }
